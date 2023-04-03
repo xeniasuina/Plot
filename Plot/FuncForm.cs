791d9c50fdd2f2b9d82c0b42478d5e9fa248d22a
MirzoevRafael <91758712+MirzoevRafael@users.noreply.github.com>
2023-04-03 18:46:46 +0300

    Update FuncForm.cs

--- Plot/FuncForm.cs
+++ Plot/FuncForm.cs
@@ -11,10 +11,10 @@ namespace Функция
         {
             if (Function.GetFunction(this.textBox1.Text))
             {
-                this.label1.Text = Function.FuncRPN;
-                this.label2.Text = "";
+                label1.Text = Function.FuncRPN;
+                label2.Text = "";
                 for (int i = -5; i < 5; i ++) this.label2.Text += Function.FuncValue(i).ToString() + ";;;    ";
-                this.label2.Text += Function.FuncValue(5).ToString();
+                label2.Text += Function.FuncValue(5).ToString();
                 return;
             }
             else new Error().ShowDialog();
